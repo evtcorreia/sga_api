7570ce3bc31e19ffa38e9245fff9a8bd9c95673c
Everton Assis Correia <evtcorreia@gmail.com>
2022-12-15 12:56:30 -0300

    Update professores.js

--- api/routes/professores.js
+++ api/routes/professores.js
@@ -10,13 +10,7 @@ router
 
 .get('/v1/professores',  verifyJwt,ProfessorController.Lista)
 .post('/v1/professor/matricular',  verifyJwt,ProfessorController.Matricular)
-.get('/v1/professor/:id',/* verifyJwt, */ ProfessorController.Buscar)
-//.put('/v1/aluno/alterar/:id',verifyJwt, AlunoController.Alterar)
-//.put('/v1/aluno/delete/:id',verifyJwt, AlunoController.Delete)
-//.get('/v1/aluno/sala/:id', verifyJwt,  AlunoController.AlunoPorSala)
-//.get('/v1/alunos/full',  verifyJwt,AlunoController.AlunosComDisciplinas)
-//.post('/v1/aluno/alocar/:id', verifyJwt, AlunoController.alocarAluno)
-//.get('/v1/aluno/turma/:id', verifyJwt, AlunoController.alunosPorTurma)
+.get('/v1/professor/:id', verifyJwt,  ProfessorController.Buscar)
 .post('/v1/professor/alocar/:id',verifyJwt,ProfessorController.AlocarProfessor)
 
 
